35d9124c9d14bae0a73d02477475430a1b26fa47
Harrison Chang <Harrison.Chang@tufts.edu>
2024-12-08 22:54:10 -0500

    Finished first draft of p5

--- p5-mbta/Sim.java
+++ p5-mbta/Sim.java
@@ -10,8 +10,25 @@ public static void run_sim(MBTA mbta, Log log) {
     Sim sim = new Sim();
     List<Thread> threads = new ArrayList<>();
 
-    for (Train train : mbta.getTrainCurrStations().keySet()) {
-        Thread t = new Thread(() -> sim.runTrain());
+    for (Train t : mbta.getTrainCurrStations().keySet()) {
+        Thread trainThread = new Thread(() -> sim.runTrain(mbta, t, log));
+        threads.add(trainThread);
+        trainThread.start();
+    }
+
+    for (Passenger p : mbta.getPassCurrStations().keySet()) {
+        Thread passThread = new Thread(() -> sim.runPassenger(mbta, p, log));
+        threads.add(passThread);
+        passThread.start();
+    }
+
+    for (Thread thread : threads) {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Simulation interrupted with error: ", e);
+        }
     }
 }
 
@@ -58,6 +75,11 @@ private void runTrain(MBTA mbta, Train t, Log log) {
                             return;
                         }
                     }
+                    mbta.setCurrTrainStation(t, nextStation);
+                    log.train_moves(t, currStation, nextStation);
+
+                    currCondition.signalAll();
+                    nextCondition.signalAll();
                 } finally {
                     nextLock.unlock();
                 }
@@ -66,7 +88,60 @@ private void runTrain(MBTA mbta, Train t, Log log) {
             }
             break;
         }
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+
+        currStationIndex = nextStationIndex;
     }
+}
+
+private void runPassenger(MBTA mbta, Passenger p, Log log) {
+    List<Station> trip = mbta.getTrip(p);
+    for (int i = 0; i < trip.size() - 1; i++) {
+        Station currStation = trip.get(i);
+        Station nextStation = trip.get(i + 1);
+
+        Lock currLock = mbta.getStationLock(currStation);
+        Condition currCondition = mbta.getStationCondition(currStation);
+        while (true) {
+            currLock.lock();
+            try {
+                Train t = mbta.getTrainAtStation(currStation);
+                if (t != null && mbta.getLine(t).contains(nextStation)) {
+                    mbta.setCurrPassTrain(p, t);
+                    log.passenger_boards(p, t, currStation);
+                    break;
+                }
+            } finally {
+                currCondition.signalAll();
+                currLock.unlock();
+            }
+        }
+
+        Lock nextLock = mbta.getStationLock(nextStation);
+        Condition nextCondition = mbta.getStationCondition(nextStation);
+        while (true) {
+            Station holdNewStation;
+            nextLock.lock();
+            try {
+                holdNewStation = mbta.getCurrTrainStation(mbta.getCurrPassTrain(p));
+                if (holdNewStation.equals(nextStation)) {
+                    mbta.setCurrPassTrain(p, null);
+                    mbta.setCurrPassStation(p, nextStation);
+                    log.passenger_deboards(p, mbta.getCurrPassTrain(p), nextStation);
+                }
+            } finally {
+                nextCondition.signalAll();
+                nextLock.unlock();
+            }
+        }
+    }
+    mbta.setPassTripComplete(p);
 }
 
 public static void main(String[] args) throws Exception {
